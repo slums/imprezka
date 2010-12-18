5ee95fa6672a466af4a53adc4ee164135a2749b6
slums <slums@slums-pavilion.(none)>
2010-12-18 22:23:42 +0100

    fixed types, integer instead of string

--- db/migrate/20101218200423_create_comments.rb
+++ db/migrate/20101218200423_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
-      t.string :user_id
-      t.string :party_id
+      t.integer :user_id
+      t.integer :party_id
       t.text :body
 
       t.timestamps
